8f215b8fd7be87737eaa28121df48a24930cf7a1
Felipe <pipelin56@hotmail.com>
2015-12-29 15:35:34 +0100

    - Se ha renombrado el fichero borrarTipos.sql a borrar.sql, ya que en este   también se borrarn las tablas. - Se ha hecho una pequeña reestructuración de los tipos y se han añadido las   tablas para los tipos definidos.

--- Tipos.sql
+++ Tipos.sql
@@ -1,18 +1,38 @@
+--Declaracion adelantada de tipos
 CREATE TYPE Tipo_Pedido;
 /
 CREATE TYPE Tipo_Linea_Detalle;
 /
-CREATE TYPE Tipo_Cliente;
-/
-CREATE TYPE Tipo_Direccion;
-/
+
 CREATE TYPE Tipo_Producto;
 /
 CREATE TYPE Tipo_Proveedor;
 /
 CREATE TYPE Tipo_Tabla_Ref_Proveedor AS TABLE OF REF Tipo_Proveedor;
 /
-
+CREATE TYPE Tipo_Direccion;
+/
+CREATE OR REPLACE TYPE Tipo_Direccion AS OBJECT(
+	Pais			VARCHAR2(30),
+	Ciudad			VARCHAR2(30),
+	Calle			VARCHAR2(20),
+	Numero			NUMBER(3),
+	CP				VARCHAR2(10)
+);
+/
+--Declaracion adelantada del tipo Tipo_Lista_Direccion
+CREATE TYPE Tipo_Lista_Direccion AS VARRAY(5) OF Tipo_Direccion;
+/
+CREATE TYPE Tipo_Cliente;
+/
+CREATE OR REPLACE TYPE	Tipo_Cliente AS OBJECT(
+	Id_Cliente				NUMBER(5),		--Realizar con secuencia
+	Dni_Cliente				VARCHAR2(9),
+	Nombre_Cliente			VARCHAR2(15),
+	Apellidos_Cliente		VARCHAR2(30),
+	Direcciones_cliente		Tipo_Lista_Direccion
+);
+/
 CREATE OR REPLACE TYPE Tipo_Linea_Detalle AS OBJECT(
 	Cantidad				NUMBER(3),
 	TieneProducto	REF 	Tipo_Producto
@@ -24,37 +44,20 @@ CREATE OR REPLACE TYPE Tipo_Pedido AS OBJECT(
 	Id_Pedido				NUMBER(5),		--Realizar con una secuencia
 	Precio_Total			NUMBER(6,2),
 	Fecha_Pedido			DATE,
-	PedidoPor		REF		Tipo_Cliente,
-	TieneLineas				Tipo_Tabla_Linea_Detalle,
+	Pedido_Por		REF		Tipo_Cliente,
+	Tiene_Lineas			Tipo_Tabla_Linea_Detalle,
 	MEMBER FUNCTION calcularTotalPedido RETURN NUMBER
 );
 /
-CREATE OR REPLACE TYPE Tipo_Direccion AS OBJECT(
-	Pais			VARCHAR2(30),
-	Ciudad			VARCHAR2(30),
-	Calle			VARCHAR2(20),
-	Numero			NUMBER(3),
-	CP				VARCHAR2(10)
-);
-/
-CREATE TYPE Tipo_Lista_Direccion AS VARRAY(5) OF Tipo_Direccion;
-/
-CREATE OR REPLACE TYPE	Tipo_Cliente AS OBJECT(
-	Id_Cliente				NUMBER(5),		--Realizar con secuencia
-	Dni_Cliente				VARCHAR2(9),
-	Nombre_Cliente			VARCHAR2(15),
-	Apellidos_Cliente		VARCHAR2(30),
-	Direcciones_cliente		Tipo_Lista_Direccion
-);
-/
+--Creacion de los tipos
 CREATE OR REPLACE TYPE Tipo_Producto AS OBJECT(
 	Stock				NUMBER(3),
 	Id_Producto			NUMBER(3),
 	Nombre_Producto		VARCHAR2(15),
 	Precio 				NUMBER(6,2),
-	ProveidoPor			Tipo_Tabla_Ref_Proveedor,
-	FINAL MEMBER FUNCTION Stock_Producto RETURN NUMBER, 
-	MEMBER FUNCTION Imprimir_Producto RETURN VARCHAR2 
+	Proveido_Por		Tipo_Tabla_Ref_Proveedor,
+	FINAL MEMBER FUNCTION StockProducto RETURN NUMBER, 
+	MEMBER FUNCTION ImprimirProducto RETURN VARCHAR2 
 ) NOT FINAL;
 /
 
@@ -67,13 +70,34 @@ CREATE OR REPLACE TYPE Tipo_Proveedor AS OBJECT(
 
 CREATE OR REPLACE TYPE Tipo_Producto_Software UNDER Tipo_Producto(
 	Licencia		VARCHAR2(15),
-	OVERRIDING MEMBER FUNCTION Imprimir_Producto RETURN VARCHAR2
+	OVERRIDING MEMBER FUNCTION ImprimirProducto RETURN VARCHAR2
 );
 /
 
 CREATE OR REPLACE TYPE Tipo_Producto_Hardware UNDER Tipo_Producto(
 	Tipo_Conexion		VARCHAR2(15),
-	OVERRIDING MEMBER FUNCTION Imprimir_Producto RETURN VARCHAR2
+	OVERRIDING MEMBER FUNCTION ImprimirProducto RETURN VARCHAR2
 );
 /
+CREATE TABLE Tabla_Linea_Detalle OF Tipo_Linea_Detalle(
+	TieneProducto NOT NULL
+);
 
+CREATE TABLE Tabla_Pedido OF Tipo_Pedido(
+	CONSTRAINT PK_Tabla_Pedido PRIMARY KEY(Id_Pedido),
+	Pedido_Por NOT NULL
+)
+NESTED TABLE Tiene_Lineas STORE AS ListaLineaDeDetalle;
+
+CREATE TABLE Tabla_Cliente OF Tipo_Cliente(
+	CONSTRAINT PK_Tabla_Cliente PRIMARY KEY(Id_Cliente)
+);
+
+CREATE TABLE Tabla_Proveedor OF Tipo_Proveedor(
+	CONSTRAINT PK_Tabla_Proveedor PRIMARY KEY(Id_Proveedor)
+);
+
+CREATE TABLE Tabla_Producto OF Tipo_Producto(
+	CONSTRAINT PK_Tabla_Producto PRIMARY KEY(Id_Producto)
+)
+NESTED TABLE Proveido_Por STORE AS ProductoProveedor;
