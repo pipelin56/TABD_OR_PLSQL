ef0f1ecf74644d1cc7561ca614cc54619cee1754
Felipe <pipelin56@hotmail.com>
2016-01-09 18:15:24 +0100

    * Cambios sobre tablas.sql:   - Se ha añadido la restriccion de UNIQUE sobre el atributo Nombre_Porducto     en las tablas: Tabla_Producto, Tabla_Prod_Software y Tabla_Prod_Hardware.

--- tablas.sql
+++ tablas.sql
@@ -33,7 +33,7 @@ NESTED TABLE Tiene_Lineas STORE AS ListaLineaDeDetalle;
 CREATE TABLE Tabla_Producto OF Tipo_Producto(
 	CONSTRAINT PK_Tabla_Producto PRIMARY KEY(Id_Producto),
 	Stock NOT NULL,
-	Nombre_Producto NOT NULL,
+	Nombre_Producto NOT NULL UNIQUE,
 	Precio_Producto NOT NULL
 )
 NESTED TABLE Proveido_Por STORE AS ProductoProveedor;
@@ -41,13 +41,15 @@ NESTED TABLE Proveido_Por STORE AS ProductoProveedor;
 --Creacion de la tabla Producto Software
 CREATE TABLE Tabla_Prod_Software OF Tipo_Producto_Software
 (
-	CONSTRAINT PK_Tabla_Prod_Software PRIMARY KEY(Id_Producto)
+	CONSTRAINT PK_Tabla_Prod_Software PRIMARY KEY(Id_Producto),
+	Nombre_Producto NOT NULL UNIQUE
 )
 NESTED TABLE Proveido_Por STORE AS ProductoProveedor_Soft;
 
 --Creacion de la tabla Producto Hardware
 CREATE TABLE Tabla_Prod_Hardware OF Tipo_Producto_Hardware
 (
-	CONSTRAINT PK_Tabla_Prod_Hardware PRIMARY KEY(Id_Producto)
+	CONSTRAINT PK_Tabla_Prod_Hardware PRIMARY KEY(Id_Producto),
+	Nombre_Producto NOT NULL UNIQUE
 )
 NESTED TABLE Proveido_Por STORE AS ProductoProveedor_Hard;
