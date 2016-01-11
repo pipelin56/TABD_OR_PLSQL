1670bc1e9bb7645bc2a04477631892576b7a996c
Felipe <pipelin56@hotmail.com>
2016-01-11 21:57:48 +0100

    Se han añadido PCTFREE y PCTUSED a las tablas.

--- tablas.sql
+++ tablas.sql
@@ -5,7 +5,9 @@ CREATE TABLE Tabla_Cliente OF Tipo_Cliente(
 	Dni_Cliente NOT NULL UNIQUE,
 	Nombre_Cliente NOT NULL,
 	Apellidos_Cliente NOT NULL
-);
+)
+PCTFREE 20
+PCTUSED 40;
 
 --Creacion de la tabla Proveedor
 CREATE TABLE Tabla_Proveedor OF Tipo_Proveedor(
@@ -13,7 +15,9 @@ CREATE TABLE Tabla_Proveedor OF Tipo_Proveedor(
 	Cif_Proveedor UNIQUE NOT NULL,
 	Nombre_Proveedor NOT NULL,
 	Direccion_Proveedor NOT NULL
-);
+)
+PCTFREE 5
+PCTUSED 40;
 
 --Creacion de la tabla Linea Detalle
 CREATE TABLE Tabla_Linea_Detalle OF Tipo_Linea_Detalle(
@@ -28,7 +32,9 @@ CREATE TABLE Tabla_Pedido OF Tipo_Pedido(
 	Fecha_Pedido NOT NULL,
 	SCOPE FOR(Pedido_Por ) IS Tabla_Cliente
 )
-NESTED TABLE Tiene_Lineas STORE AS ListaLineaDeDetalle;
+NESTED TABLE Tiene_Lineas STORE AS ListaLineaDeDetalle
+PCTFREE 5
+PCTUSED 40;
 
 --Creacion de la tabla Producto
 CREATE TABLE Tabla_Producto OF Tipo_Producto(
@@ -37,7 +43,9 @@ CREATE TABLE Tabla_Producto OF Tipo_Producto(
 	Nombre_Producto NOT NULL UNIQUE,
 	Precio_Producto NOT NULL
 )
-NESTED TABLE Proveido_Por STORE AS ProductoProveedor;
+NESTED TABLE Proveido_Por STORE AS ProductoProveedor
+PCTFREE 5
+PCTUSED 40;
 
 ALTER TABLE ProductoProveedor ADD
 (SCOPE FOR (column_value) IS Tabla_Proveedor);
@@ -48,7 +56,9 @@ CREATE TABLE Tabla_Prod_Software OF Tipo_Producto_Software
 	CONSTRAINT PK_Tabla_Prod_Software PRIMARY KEY(Id_Producto),
 	Nombre_Producto NOT NULL UNIQUE
 )
-NESTED TABLE Proveido_Por STORE AS ProductoProveedor_Soft;
+NESTED TABLE Proveido_Por STORE AS ProductoProveedor_Soft
+PCTFREE 5
+PCTUSED 40;
 
 --Creacion de la tabla Producto Hardware
 CREATE TABLE Tabla_Prod_Hardware OF Tipo_Producto_Hardware
@@ -56,4 +66,6 @@ CREATE TABLE Tabla_Prod_Hardware OF Tipo_Producto_Hardware
 	CONSTRAINT PK_Tabla_Prod_Hardware PRIMARY KEY(Id_Producto),
 	Nombre_Producto NOT NULL UNIQUE
 )
-NESTED TABLE Proveido_Por STORE AS ProductoProveedor_Hard;
+NESTED TABLE Proveido_Por STORE AS ProductoProveedor_Hard
+PCTFREE 5
+PCTUSED 40;
